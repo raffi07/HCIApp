b14ed978a3b2512d4c7d4b503fbd12133ace20b6
Raphael Imfeld <raphael.imfeld@uzh.ch>
2019-12-06 22:28:22 +0100

    initial

---
+++ app/src/main/java/com/example/teachin/LoadQR.java
@@ -0,0 +1,14 @@
+package com.example.teachin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_qr);
+    }
+}
--- app/src/main/java/com/example/teachin/MainActivity.java
+++ app/src/main/java/com/example/teachin/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.teachin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button informationButton;
+    private Button responseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.informationButton = findViewById(R.id.button);
+        this.responseButton = findViewById(R.id.button2);
+
+        informationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoadScreen("information");
+            }
+        });
+        responseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoadScreen("response");
+            }
+        });
+    }
+
+    private void openLoadScreen(String type){
+        Intent intent = new Intent(this, LoadQR.class);
+        intent.putExtra(type, LoadQR.class);
+        startActivity(intent);
+
     }
 }
