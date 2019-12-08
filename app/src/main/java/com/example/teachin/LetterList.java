5b9e6687d979ebd0010743837eb6530dff31cc98
Raphael Imfeld <raphael.imfeld@uzh.ch>
2019-12-08 10:10:45 +0100

    Click for list almost set up

--- app/src/main/java/com/example/teachin/LetterList.java
+++ app/src/main/java/com/example/teachin/LetterList.java
@@ -9,8 +9,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class LetterList extends AppCompatActivity {
+    private ArrayList<LetterListItem> letterList;
+
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private MyAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
     @Override
@@ -18,22 +20,45 @@ public class LetterList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letter_list);
 
-        ArrayList<LetterListItem> letterList = new ArrayList<>();
-        letterList.add(new LetterListItem(
-                R.drawable.ic_infodocument,
-                getResources().getString(R.string.listFirstEntry),
-                getResources().getString(R.string.listFirstEntryCount)));
-        letterList.add(new LetterListItem(
+        createItemList();
+        buildRecyclerView();
+    }
+
+    public void insertItem(){
+        this.letterList.add(0, new LetterListItem(
                 R.drawable.ic_responsedocument,
-                getResources().getString(R.string.listSecondEntry),
-                getResources().getString(R.string.listSecondEntryCount)));
+                getResources().getString(R.string.listThirdEntry),
+                getResources().getString(R.string.listThirdEntryUpdated)));
+    }
 
-        recyclerView = findViewById(R.id.letterList);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        mAdapter = new MyAdapter(letterList);
+    public void createItemList(){
+        this.letterList= new ArrayList<>();
+            letterList.add(new LetterListItem(
+                    R.drawable.ic_infodocument,
+                    getResources().getString(R.string.listFirstEntry),
+                    getResources().getString(R.string.listFirstEntryCount)));
+            letterList.add(new LetterListItem(
+                    R.drawable.ic_responsedocument,
+                    getResources().getString(R.string.listSecondEntry),
+                    getResources().getString(R.string.listSecondEntryCount)));
+    }
 
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(mAdapter);
+    public void buildRecyclerView(){
+    recyclerView = findViewById(R.id.letterList);
+    recyclerView.setHasFixedSize(true);
+    layoutManager = new LinearLayoutManager(this);
+    mAdapter = new MyAdapter(letterList);
+
+    recyclerView.setLayoutManager(layoutManager);
+    recyclerView.setAdapter(mAdapter);
+
+    mAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+        @Override
+        public void onItemClick(int position) {
+            if(position == 0){
+
+            };
+        }
+    });
     }
 }
--- app/src/main/java/com/example/teachin/MyAdapter.java
+++ app/src/main/java/com/example/teachin/MyAdapter.java
@@ -5,6 +5,7 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -14,6 +15,15 @@ import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
     private ArrayList<LetterListItem> itemArray;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         public ImageView imageView;
@@ -21,11 +31,23 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
         public TextView secondEntry;
 
 
-        public MyViewHolder(View itemView){
+        public MyViewHolder(View itemView, final OnItemClickListener listener){
             super(itemView);
             imageView = itemView.findViewById(R.id.itemCard);
             firstEntry = itemView.findViewById(R.id.documentTitle);
             secondEntry = itemView.findViewById(R.id.studentCount);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 
@@ -37,7 +59,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        MyViewHolder myViewHolder = new MyViewHolder(v);
+        MyViewHolder myViewHolder = new MyViewHolder(v, this.listener);
         return myViewHolder;
     }
 
