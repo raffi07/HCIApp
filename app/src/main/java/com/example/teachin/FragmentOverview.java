09d0f17af520819a6f5cb52ee7cf40f5d6e8b62d
Raphael Imfeld <raphael.imfeld@uzh.ch>
2019-12-08 19:42:09 +0100

    Overview info letter

--- app/src/main/java/com/example/teachin/FragmentOverview.java
+++ app/src/main/java/com/example/teachin/FragmentOverview.java
@@ -7,9 +7,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class FragmentOverview extends Fragment {
     private View v;
+
+    private ArrayList<LetterListItem> childrenList;
+
+    private RecyclerView recyclerView;
+    private MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public FragmentOverview() {
     }
@@ -18,6 +28,40 @@ public class FragmentOverview extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.overview_fragment,container,false);
+
+        createItemList();
+        buildRecyclerView(v);
+
         return v;
+    }
+
+    public void createItemList(){
+        this.childrenList= new ArrayList<>();
+        childrenList.add(new LetterListItem(
+                R.drawable.ic_completed,
+                "Anna",
+                "1 day ago"));
+        childrenList.add(new LetterListItem(
+                R.drawable.ic_sent,
+                "Bob",
+                "5 days ago"));
+        childrenList.add(new LetterListItem(
+                R.drawable.ic_completed,
+                "Catherina",
+                "1 day ago"));
+        childrenList.add(new LetterListItem(
+                R.drawable.ic_completed,
+                "David",
+                "today"));
+    }
+
+    public void buildRecyclerView(View v){
+        recyclerView = (RecyclerView) v.findViewById(R.id.childrenList);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new MyAdapter(childrenList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(mAdapter);
     }
 }
